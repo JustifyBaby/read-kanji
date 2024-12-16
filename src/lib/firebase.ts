0d9a97d02004a69f2c49d7aacb619cb8b8603f7d
JustifyBaby <quronoa1232@gmail.com>
2024-12-17 07:41:44 +0900

    ignore

--- src/lib/firebase.ts
+++
@@ -1,9 +0,0 @@
-// const firebaseConfig = {
-//   apiKey: String(process.env.NEXT_PUBLIC_API_KEY),
-//   authDomain: String(process.env.NEXT_PUBLIC_AUTH_DOMAIN),
-//   projectId: String(process.env.NEXT_PUBLIC_PROJECT_URL),
-//   storageBucket: String(process.env.NEXT_PUBLIC_STORAGE),
-//   messagingSenderId: String(process.env.NEXT_PUBLIC_MSG_SENDER_ID),
-//   appId: String(process.env.NEXT_PUBLIC_APP_ID),
-//   measurementId: String(process.env.NEXT_PUBLIC_MEASURE_ID),
-// };
